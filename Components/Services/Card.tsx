245ad8eb6433ee9e1af40454e181586e48cdc36f
safiullah77777 <safiullahrafeeq7777@gmail.com>
2023-04-13 17:06:24 +0500

    design changes

--- Components/Services/Card.tsx
+++ Components/Services/Card.tsx
@@ -1,4 +1,4 @@
-import { StaticImageData } from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
 const Card = ({
@@ -11,7 +11,11 @@ const Card = ({
   desc: string;
 }) => {
   return (
-    <div className="shadow-card bg-[#fff] p-[2rem] w-[28.7rem] h-[28.3rem] rounded-[.8rem] flex flex-col gap-[2rem]"></div>
+    <div className="shadow-card bg-[#fff] p-[2rem] w-[28.7rem] h-[28.3rem] rounded-[.8rem] flex flex-col gap-[2rem]">
+        <Image src={src} alt='' />
+        <span className="text-[3rem] font-500 leading-[3.6rem] text-[#263238]">{title}</span>
+        <span className="text-[1.6rem] font-400 leading-[1.9rem] text-[#000000]">{desc}</span>
+    </div>
   );
 };
 
--- Components/Services/index.tsx
+++ Components/Services/index.tsx
@@ -2,8 +2,35 @@ import React from "react";
 import connect from "../../public/assets/images/connect.png";
 import promote from "../../public/assets/images/promote.png";
 import paid from "../../public/assets/images/paid.png";
+import Card from "./Card";
 
 const Services = () => {
+  const data = [
+    {
+      img: {
+        src: connect,
+        alt: "",
+      },
+      title: "Connect",
+      desc: "We connect you with a local brand on your campus",
+    },
+    {
+      img: {
+        src: paid,
+        alt: "",
+      },
+      title: "Promote",
+      desc: "Work together and run promotions with local businesses on campus",
+    },
+    {
+      img: {
+        src: paid,
+        alt: "",
+      },
+      title: "Get Paid",
+      desc: "Get Paid and grow the brands you live in your town",
+    },
+  ];
   return (
     <div className="bg-primary pt-[6.1rem] pb-[6.8rem] px-[3rem] flex flex-col items-center justify-center gap-[6rem] !z-[111121]">
       <h3 className="text-[3.6rem] text-[#fff] leading-[4.4rem] flex flex-col font-600 pb-[1.5rem] relative after:absolute after:h-[.5rem] after:w-[10.7rem] after:bottom-0 after:rounded-[2rem] rounded-[2rem] after:left-[2rem] after:bg-[#fff] z-[2]">
@@ -13,8 +40,15 @@ const Services = () => {
         Connect with Brands to monetize your niched audience Match with students
         to target customers effectively utilizing niched student audiences
       </p>
-      <div className="flex gap-[7.6rem]">
-
+      <div className="flex justify-center gap-[7.6rem]">
+        {data.map((item, index) => (
+          <Card
+            desc={item.desc}
+            src={item.img.src}
+            title={item.title}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
